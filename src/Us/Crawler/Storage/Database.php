2a1fa0c46beb403052a94b946c6cdf5c5a897ad4
Tuvix Shih <tuvix@ninthday.info>
2018-08-21 16:04:51 +0800

    增加 db-host 參數

--- src/Us/Crawler/Storage/Database.php
+++ src/Us/Crawler/Storage/Database.php
@@ -3,47 +3,49 @@ use \PDO;
 
 class Database
 {
-	public $db = null;
-	private $_db_username = '';
-	private $_db_password = '';
+    public $db = null;
+    private $_db_username = '';
+    private $_db_password = '';
+    private $_db_host = '';
 
-	function __construct($db_username, $db_password = '')
-	{
-		$this->_db_username = $db_username;
-		$this->_db_password = $db_password;
-		$this->openPDO();
-	}
+    function __construct($db_username, $db_password = '', $db_host = '127.0.0.1')
+    {
+        $this->_db_username = $db_username;
+        $this->_db_password = $db_password;
+        $this->_db_host = $db_host;
+        $this->openPDO();
+    }
 
-	private function openPDO()
-	{
-		// connection info for PDO
-		define("DB_TYPE", "mysql");
-		define("DB_HOST", "127.0.0.1");
-		define("DB_NAME", "ptt_crawler");
-		define("DB_USER", $this->_db_username);
-		define("DB_PASS", $this->_db_password);
+    private function openPDO()
+    {
+        // connection info for PDO
+        define("DB_TYPE", "mysql");
+        define("DB_HOST", $this->_db_host);
+        define("DB_NAME", "ptt_crawler");
+        define("DB_USER", $this->_db_username);
+        define("DB_PASS", $this->_db_password);
 
-		// error code of PDO
-		define("SERVER_SHUTDOWN_CODE", "1053");
+        // error code of PDO
+        define("SERVER_SHUTDOWN_CODE", "1053");
 
-		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
-		try {
-			$this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
-			$this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+        try {
+            $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
+            $this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $e) {
-			exit("Database connection error\n");
-		}
-	}
+        } catch (PDOException $e) {
+            exit("Database connection error\n");
+        }
+    }
 
-	public function reconnectPDO()
-	{
-		$this->db = null;
-		$this->openPDO();
-	}
+    public function reconnectPDO()
+    {
+        $this->db = null;
+        $this->openPDO();
+    }
 
-	public function __destruct()
-	{
-		$this->db = null;
-	}
+    public function __destruct()
+    {
+        $this->db = null;
+    }
 }
